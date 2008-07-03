c32e069010720355d0e276724e488b245106e25b
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-07-03 06:40:12 +0000

    git-svn-id: https://clrextensions.svn.codeplex.com/svn@13533 2327b42d-5241-43d6-9e2a-de5ac946f064

--- ClrExtensionsCore/System/EventHandlerExtension.vb
+++ ClrExtensionsCore/System/EventHandlerExtension.vb
@@ -14,4 +14,15 @@ Public Module EventHandlerExtension
 		If this IsNot Nothing Then this(sender, e)
 	End Sub
 
+	''' <summary>
+	''' This method is only available to C# applications. VB programs should use the RaiseEvent keyword instead.
+	''' </summary>
+	''' <param name="this"></param>
+	''' <param name="sender"></param>
+	''' <remarks></remarks>
+	<MethodImpl(MethodImplOptions.NoInlining)> _
+	<Extension()> Public Sub [RaiseEvent](ByVal this As EventHandler(Of EventArgs), ByVal sender As Object)
+		Threading.Thread.MemoryBarrier()
+		If this IsNot Nothing Then this(sender, EventArgs.Empty)
+	End Sub
 End Module
