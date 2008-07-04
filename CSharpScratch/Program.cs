55d8fab449f16a43723356cfd908004c207656b7
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-07-04 08:27:10 +0000

    git-svn-id: https://clrextensions.svn.codeplex.com/svn@13706 2327b42d-5241-43d6-9e2a-de5ac946f064

--- CSharpScratch/Program.cs
+++ CSharpScratch/Program.cs
@@ -10,11 +10,35 @@ namespace CSharpScratch
 	{
 		static EventHandler<EventArgs> Boom;
 
+
+		static void commit() { }
+		static void report_that_b_failed() { }
+		static void report_that_a_failed() { }
+
 		static void Main(string[] args)
 		{
-			Boom.RaiseEvent(null, EventArgs.Empty);
-			Boom += CatchBoom;
-			Boom.RaiseEvent(null, EventArgs.Empty);
+			var d = new Dictionary<int, int, Action>();
+			d.Add(1, 1, Commit);
+			d.Add(1, 0, report_that_b_failed);
+			d.Add(0, 0, report_that_a_failed);
+
+			var a = 1, b = 0;
+			d[a, b].Invoke();
+
+
+
+			X.Add(5, true, () => "Boom!");
+			X.Add(5, false, () => "Splat!");
+			X.Add(15, true, () => "Ka-Pow");
+
+			Console.WriteLine(X[5, true].Invoke());
+			Console.WriteLine(X[5, false].Invoke());
+			Console.WriteLine(X[15, false].Invoke());
+
+
+			//Boom.RaiseEvent(null, EventArgs.Empty);
+			//Boom += CatchBoom;
+			//Boom.RaiseEvent(null, EventArgs.Empty);
 		}
 
 		static void CatchBoom(object sender, EventArgs e)
