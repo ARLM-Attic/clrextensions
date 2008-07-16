aff79ce4c919c2579fa2149cb824e57ca713f0f1
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-07-16 03:01:18 +0000

    git-svn-id: https://clrextensions.svn.codeplex.com/svn@14797 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ ClrExtensions Auotmated Tests/DecimalExtensionTest.vb
@@ -0,0 +1,104 @@
+﻿Imports System
+
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports ClrExtensions
+
+
+
+'''<summary>
+'''This is a test class for DecimalExtensionTest and is intended
+'''to contain all DecimalExtensionTest Unit Tests
+'''</summary>
+<TestClass()> _
+Public Class DecimalExtensionTest
+
+
+	Private testContextInstance As TestContext
+
+	'''<summary>
+	'''Gets or sets the test context which provides
+	'''information about and functionality for the current test run.
+	'''</summary>
+	Public Property TestContext() As TestContext
+		Get
+			Return testContextInstance
+		End Get
+		Set(ByVal value As TestContext)
+			testContextInstance = Value
+		End Set
+	End Property
+
+#Region "Additional test attributes"
+	'
+	'You can use the following additional attributes as you write your tests:
+	'
+	'Use ClassInitialize to run code before running the first test in the class
+	'<ClassInitialize()>  _
+	'Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+	'End Sub
+	'
+	'Use ClassCleanup to run code after all tests in a class have run
+	'<ClassCleanup()>  _
+	'Public Shared Sub MyClassCleanup()
+	'End Sub
+	'
+	'Use TestInitialize to run code before running each test
+	'<TestInitialize()>  _
+	'Public Sub MyTestInitialize()
+	'End Sub
+	'
+	'Use TestCleanup to run code after each test has run
+	'<TestCleanup()>  _
+	'Public Sub MyTestCleanup()
+	'End Sub
+	'
+#End Region
+
+
+	Dim m_List As Decimal() = New Decimal() {12345.67889D, 123.456D, 12.34D, 1.2D, 0, Decimal.MinusOne, Decimal.One, Decimal.MaxValue, Decimal.MinValue}
+
+	'''<summary>
+	'''A test for TruncatePrecision
+	'''</summary>
+	<TestMethod()> _
+	Public Sub TruncatePrecisionTest()
+
+		Dim expected As New Collections.Generic.Dictionary(Of Integer, Decimal())
+
+		expected.Add(5, New Decimal() {12345.67889D, 123.456D, 12.34D, 1.2D, 0, -1, 1, 79228162514264337593543950335D, -79228162514264337593543950335D})
+		expected.Add(4, New Decimal() {12345.6788D, 123.456D, 12.34D, 1.2D, 0, -1, 1, 79228162514264337593543950335D, -79228162514264337593543950335D})
+		expected.Add(3, New Decimal() {12345.678D, 123.456D, 12.34D, 1.2D, 0, -1, 1, 79228162514264337593543950335D, -79228162514264337593543950335D})
+		expected.Add(2, New Decimal() {12345.67D, 123.45D, 12.34D, 1.2D, 0, -1, 1, 79228162514264337593543950335D, -79228162514264337593543950335D})
+		expected.Add(1, New Decimal() {12345.6D, 123.4D, 12.3D, 1.2D, 0, -1, 1, 79228162514264337593543950335D, -79228162514264337593543950335D})
+		expected.Add(0, New Decimal() {12345D, 123D, 12D, 1D, 0, -1, 1, 79228162514264337593543950335D, -79228162514264337593543950335D})
+		expected.Add(-1, New Decimal() {12340D, 120D, 10D, 0, 0, 0, 0, 79228162514264337593543950330D, -79228162514264337593543950330D})
+		expected.Add(-2, New Decimal() {12300D, 100D, 0D, 0, 0, 0, 0, 79228162514264337593543950300D, -79228162514264337593543950300D})
+		expected.Add(-3, New Decimal() {12000D, 0D, 0D, 0, 0, 0, 0, 79228162514264337593543950000D, -79228162514264337593543950000D})
+		expected.Add(-4, New Decimal() {10000D, 0D, 0D, 0D, 0, 0, 0, 79228162514264337593543950000D, -79228162514264337593543950000D})
+		expected.Add(-5, New Decimal() {0D, 0D, 0D, 0D, 0, 0, 0, 79228162514264337593543900000D, -79228162514264337593543900000D})
+
+		For Each key In expected.Keys
+			For i = 0 To m_List.Length - 1
+				Assert.AreEqual(expected(key)(i), m_List(i).TruncatePrecision(key))
+			Next
+		Next
+
+	End Sub
+
+	'''<summary>
+	'''A test for Pow10
+	'''</summary>
+	<TestMethod()> _
+ Public Sub Pow10Test()
+		Dim expected = New Decimal() {0.00001D, 0.0001D, 0.001D, 0.01D, 0.1D, 1, 10, 100, 1000, 10000, 100000}
+
+		For i = -5 To 5
+			Assert.AreEqual(expected(i + 5), DecimalExtension.Pow10(i))
+		Next
+
+	End Sub
+End Class
+
+
+
--- ClrExtensionsCore/System/DecimalExtension.vb
+++ ClrExtensionsCore/System/DecimalExtension.vb
@@ -1,15 +1,14 @@
-#If IncludeUntested Then
 
-<Untested()> Public Module DecimalExtension
+Public Module DecimalExtension
 
-	Private Function Pow10(ByVal exponent As Integer) As Decimal
+	Public Function Pow10(ByVal exponent As Integer) As Decimal
 		Dim result As Decimal = 1
 		If exponent > 0 Then
 			For i = 1 To exponent
 				result *= 10
 			Next
 		ElseIf exponent < 0 Then
-			For i = exponent To -1 Step -1
+			For i = exponent To -1
 				result /= 10
 			Next
 		End If
@@ -22,14 +21,27 @@
 	''' <param name="value"></param>
 	''' <param name="precision">Number of decimal places to retain. If negative, number of zeros to the left of the decimal place.</param>
 	''' <returns></returns>
-	''' <remarks>This may inadvertendly throw an overflow exception for large values with high precision</remarks>
+	''' <remarks>This may inadvertendly throw an overflow exception for large values</remarks>
 	<Extension()> Public Function TruncatePrecision(ByVal value As Decimal, ByVal precision As Integer) As Decimal
-		Dim precisionPower = Pow10(precision)
-		Return Decimal.Truncate(value * precisionPower) / precisionPower
+		Try
+			Dim precisionPower = Pow10(precision)
+			Return Decimal.Truncate(value * precisionPower) / precisionPower
+		Catch ex As OverflowException
+			'Revert to slow method
+			Dim literal = value.ToString
 
-		'TODO: Rewrite this so it will work with any decimal
+			Select Case precision
+				Case Is > 0
+					Dim dotIndex = literal.IndexOf(".")
+					If dotIndex + precision + 1 > literal.Length Then Return CDec(literal.Substring(0, dotIndex + precision + 1))
+					Return value
+				Case 0
+					Return CDec(literal.Substring(0, literal.IndexOf(".")))
+				Case Is < 0
+					Return CDec(literal.Substring(0, literal.IndexOf(".") + precision) & "0".Repeat(-1 * precision))
+			End Select
+		End Try
 	End Function
 
 End Module
 
-#End If
