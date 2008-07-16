8bac4995bb7a850e5eee0b3694ba2357ee18850e
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-07-16 23:50:58 +0000

    git-svn-id: https://clrextensions.svn.codeplex.com/svn@14890 2327b42d-5241-43d6-9e2a-de5ac946f064

--- CSharpScratch/Program.cs
+++ CSharpScratch/Program.cs
@@ -11,29 +11,29 @@ namespace CSharpScratch
     {
         static void Main(string[] args)
         {
-            var arr = new String[3, 3];
+            //var arr = new String[3, 3];
 
 
-            arr[0, 0] = "Carl";
-            arr[1, 0] = "Adam";
-            arr[2, 0] = "Scott";
-            arr[0, 1] = "Fred";
-            arr[1, 1] = "Louis";
-            arr[2, 1] = "Bob";
-            arr[0, 2] = "Simon";
-            arr[1, 2] = "Joe";
-            arr[2, 2] = "Bill";
+            //arr[0, 0] = "Carl";
+            //arr[1, 0] = "Adam";
+            //arr[2, 0] = "Scott";
+            //arr[0, 1] = "Fred";
+            //arr[1, 1] = "Louis";
+            //arr[2, 1] = "Bob";
+            //arr[0, 2] = "Simon";
+            //arr[1, 2] = "Joe";
+            //arr[2, 2] = "Bill";
 
-            var sorted = arr.SortByColumn(0);
+            ////var sorted = arr.SortByColumn(0);
 
-            for (var i = 0; i < 3; i++)
-            {
-                for (var j = 0; j < 3; j++)
-                {
-                    Console.Write(sorted[i, j] + ' ');
-                } Console.WriteLine();
-            }
-            Console.ReadLine();
+            //for (var i = 0; i < 3; i++)
+            //{
+            //    for (var j = 0; j < 3; j++)
+            //    {
+            //        Console.Write(sorted[i, j] + ' ');
+            //    } Console.WriteLine();
+            //}
+            //Console.ReadLine();
         }
     }
 
