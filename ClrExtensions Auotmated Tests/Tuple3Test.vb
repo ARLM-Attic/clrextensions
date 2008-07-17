146e660b50302fc4bd8ac2fc5c7bb17e268d6656
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-07-17 00:48:47 +0000

    git-svn-id: https://clrextensions.svn.codeplex.com/svn@14896 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ ClrExtensions Auotmated Tests/Tuple3Test.vb
@@ -0,0 +1,158 @@
+﻿Imports System
+
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+Imports ClrExtensions.System.Collections
+
+
+
+'''<summary>
+'''This is a test class for Tuple3Test and is intended
+'''to contain all Tuple3Test Unit Tests
+'''</summary>
+<TestClass()> _
+Public Class Tuple3Test
+
+
+    Private testContextInstance As TestContext
+
+    '''<summary>
+    '''Gets or sets the test context which provides
+    '''information about and functionality for the current test run.
+    '''</summary>
+    Public Property TestContext() As TestContext
+        Get
+            Return testContextInstance
+        End Get
+        Set(ByVal value As TestContext)
+            testContextInstance = Value
+        End Set
+    End Property
+
+#Region "Additional test attributes"
+    '
+    'You can use the following additional attributes as you write your tests:
+    '
+    'Use ClassInitialize to run code before running the first test in the class
+    '<ClassInitialize()>  _
+    'Public Shared Sub MyClassInitialize(ByVal testContext As TestContext)
+    'End Sub
+    '
+    'Use ClassCleanup to run code after all tests in a class have run
+    '<ClassCleanup()>  _
+    'Public Shared Sub MyClassCleanup()
+    'End Sub
+    '
+    'Use TestInitialize to run code before running each test
+    '<TestInitialize()>  _
+    'Public Sub MyTestInitialize()
+    'End Sub
+    '
+    'Use TestCleanup to run code after each test has run
+    '<TestCleanup()>  _
+    'Public Sub MyTestCleanup()
+    'End Sub
+    '
+#End Region
+
+
+
+
+    Private ReadOnly ValueA As New Tuple(Of Integer, String, Boolean)(5, "AAA", True)
+    Private ReadOnly ValueB As New Tuple(Of Integer, String, Boolean)(7, "BBB", False)
+    Private ReadOnly ValueC1 As New Tuple(Of Integer, String, Boolean)(9, "CCC", True)
+    Private ReadOnly ValueC2 As New Tuple(Of Integer, String, Boolean)(9, "CCC", True)
+    Private ReadOnly ValueZ As New Tuple(Of Integer, String, Boolean)(0, Nothing, False)
+
+    Dim aList() As Integer = New Integer() {5, 7, 9, 9, 0}
+    Dim bList() As String = New String() {"AAA", "BBB", "CCC", "CCC", Nothing}
+    Dim cList() As Boolean = New Boolean() {True, False, True, True, False}
+    Dim list() As Tuple(Of Integer, String, Boolean) = New Tuple(Of Integer, String, Boolean)() {ValueA, ValueB, ValueC1, ValueC2, ValueZ}
+
+
+
+
+
+    <TestMethod()> _
+    Public Sub CTest()
+        For i = 0 To list.Length - 1
+            Assert.AreEqual(cList(i), list(i).C)
+        Next
+    End Sub
+
+    <TestMethod()> _
+    Public Sub BTest()
+        For i = 0 To list.Length - 1
+            Assert.AreEqual(bList(i), list(i).B)
+        Next
+    End Sub
+
+
+
+    <TestMethod()> _
+    Public Sub ATest()
+        For i = 0 To list.Length - 1
+            Assert.AreEqual(aList(i), list(i).A)
+        Next
+    End Sub
+
+
+    <TestMethod()> _
+    Public Sub GetHashCodeTest()
+        For i = 0 To list.Length - 1
+            For j = 0 To list.Length - 1
+
+                If i = 2 And j = 3 Or i = 3 And j = 2 Then
+                    Assert.IsTrue(list(i).GetHashCode = list(j).GetHashCode)
+                ElseIf i = j Then
+                    Assert.IsTrue(list(i).GetHashCode = list(j).GetHashCode)
+                Else
+                    'Undefined
+                End If
+            Next
+        Next
+    End Sub
+
+    <TestMethod()> _
+    Public Sub EqualsTest1()
+        For i = 0 To list.Length - 1
+            For j = 0 To list.Length - 1
+
+                If i = 2 And j = 3 Or i = 3 And j = 2 Then
+                    Assert.IsTrue(Object.Equals(list(i), list(j)))
+                ElseIf i = j Then
+                    Assert.IsTrue(Object.Equals(list(i), list(j)))
+                Else
+                    Assert.IsFalse(Object.Equals(list(i), list(j)))
+                End If
+            Next
+        Next
+    End Sub
+
+
+
+    <TestMethod()> _
+    Public Sub EqualsTest()
+
+        For i = 0 To list.Length - 1
+            For j = 0 To list.Length - 1
+
+                If i = 2 And j = 3 Or i = 3 And j = 2 Then
+                    Assert.IsTrue(list(i).Equals(list(j)))
+                ElseIf i = j Then
+                    Assert.IsTrue(list(i).Equals(list(j)))
+                Else
+                    Assert.IsFalse(list(i).Equals(list(j)))
+                End If
+            Next
+        Next
+
+    End Sub
+
+    <TestMethod()> _
+    Public Sub TupleConstructorTest()
+        'handled by BTest and ATest
+    End Sub
+
+    
+End Class
--- ClrExtensionsCore/System/Collections/Tuple2.vb
+++ ClrExtensionsCore/System/Collections/Tuple2.vb
@@ -1,48 +1,45 @@
-#If IncludeUntested Then
-
 Namespace System.Collections
 
-	<Untested()> Public Structure Tuple(Of TA, TB)
-		Implements IEquatable(Of Tuple(Of TA, TB))
+    Public Class Tuple(Of TA, TB)
+        Implements IEquatable(Of Tuple(Of TA, TB))
 
-		Private m_A As TA
-		Private m_B As TB
+        Private m_A As TA
+        Private m_B As TB
 
-		Public ReadOnly Property B() As TB
-			Get
-				Return m_B
-			End Get
-		End Property
+        Public ReadOnly Property B() As TB
+            Get
+                Return m_B
+            End Get
+        End Property
 
-		Public ReadOnly Property A() As TA
-			Get
-				Return m_A
-			End Get
-		End Property
+        Public ReadOnly Property A() As TA
+            Get
+                Return m_A
+            End Get
+        End Property
 
-		Public Sub New(ByVal a As TA, ByVal b As TB)
-			m_A = a
-			m_B = b
-		End Sub
+        Public Sub New(ByVal a As TA, ByVal b As TB)
+            m_A = a
+            m_B = b
+        End Sub
 
-		Public Overrides Function Equals(ByVal obj As Object) As Boolean
-			If Not TypeOf obj Is Tuple(Of TA, TB) Then Return False
-			Return Me.Equals(DirectCast(obj, Tuple(Of TA, TB)))
-		End Function
+        Public Overrides Function Equals(ByVal obj As Object) As Boolean
+            If Not TypeOf obj Is Tuple(Of TA, TB) Then Return False
+            Return Me.Equals(DirectCast(obj, Tuple(Of TA, TB)))
+        End Function
 
-		Public Overrides Function GetHashCode() As Integer
-			Dim result = 0
+        Public Overrides Function GetHashCode() As Integer
+            Dim result = 0
 
-			If m_A IsNot Nothing Then result = result Xor m_A.GetHashCode
-			If m_B IsNot Nothing Then result = result Xor m_B.GetHashCode
+            If m_A IsNot Nothing Then result = result Xor m_A.GetHashCode
+            If m_B IsNot Nothing Then result = result Xor m_B.GetHashCode
 
-			Return result
-		End Function
+            Return result
+        End Function
 
-		Public Overloads Function Equals(ByVal other As Tuple(Of TA, TB)) As Boolean Implements IEquatable(Of Tuple(Of TA, TB)).Equals
-			Return Object.Equals(A, other.A) AndAlso Object.Equals(B, other.B)
-		End Function
-	End Structure
+        Public Overloads Function Equals(ByVal other As Tuple(Of TA, TB)) As Boolean Implements IEquatable(Of Tuple(Of TA, TB)).Equals
+            Return Object.Equals(A, other.A) AndAlso Object.Equals(B, other.B)
+        End Function
+    End Class
 
 End Namespace
-#End If
