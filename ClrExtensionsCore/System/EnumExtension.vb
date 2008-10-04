f165c263942e092c2e1a0c83ccd3c49dba7064f8
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-10-04 04:55:24 +0000

    git-svn-id: https://clrextensions.svn.codeplex.com/svn@17977 2327b42d-5241-43d6-9e2a-de5ac946f064

--- ClrExtensionsCore/System/EnumExtension.vb
+++ ClrExtensionsCore/System/EnumExtension.vb
@@ -10,7 +10,34 @@ Public Module EnumExtension
 	Public Function IsDefined(Of T As Structure)(ByVal value As T) As Boolean
 		If Not GetType(T).IsEnum Then Throw New ArgumentException("T is not an enumeration")
 		Return [Enum].IsDefined(GetType(T), value)
-	End Function
+    End Function
+
+#If IncludeUntested Then
+
+    <Untested()> Public Function EnumParse(Of T As Structure)(ByVal value As String) As T
+        Return CType([Enum].Parse(GetType(T), value), T)
+    End Function
+
+    <Untested()> Public Function EnumTryParse(Of T As Structure)(ByVal value As String) As T?
+        Try
+            Return CType([Enum].Parse(GetType(T), value), T)
+        Catch ex As ArgumentException
+            Return Nothing
+        End Try
+    End Function
+
+    <Untested()> Public Function EnumParse(Of T As Structure)(ByVal value As String, ByVal ignoreCase As Boolean) As T
+        Return CType([Enum].Parse(GetType(T), value, ignoreCase), T)
+    End Function
+
+    <Untested()> Public Function EnumTryParse(Of T As Structure)(ByVal value As String, ByVal ignoreCase As Boolean) As T?
+        Try
+            Return CType([Enum].Parse(GetType(T), value, ignoreCase), T)
+        Catch ex As ArgumentException
+            Return Nothing
+        End Try
+    End Function
+#End If
 
 End Module
 
