ffca1f42ea0408355bbbdc55ee9c3a67ba54e347
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-10-04 09:46:03 +0000

    Added IDisposable classes so we can use the "Using lock.ReadSection" syntax instead of the Enter-Try-Finally syntax.
    git-svn-id: https://clrextensions.svn.codeplex.com/svn@17980 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ ClrExtensionsCore/System/Threading/ReaderWriterLockSlimExtension.vb
@@ -0,0 +1,65 @@
+﻿#If IncludeUntested Then
+Public Module ReaderWriterLockSlimExtension
+
+	<Untested()> <Extension()> Public Function ReadSection(ByVal lock As Global.System.Threading.ReaderWriterLockSlim) As IDisposable
+		Return New LockWrapper(lock, LockMode.Read)
+	End Function
+
+	<Untested()> <Extension()> Public Function UpgradeableReadSection(ByVal lock As Global.System.Threading.ReaderWriterLockSlim) As IDisposable
+		Return New LockWrapper(lock, LockMode.Upgradable)
+	End Function
+
+	<Untested()> <Extension()> Public Function WriteSection(ByVal lock As Global.System.Threading.ReaderWriterLockSlim) As IDisposable
+		Return New LockWrapper(lock, LockMode.Write)
+	End Function
+
+	Private NotInheritable Class LockWrapper
+		Implements IDisposable
+		Private m_Mode As LockMode
+		Private m_Lock As Global.System.Threading.ReaderWriterLockSlim
+		Private m_Disposed As Boolean = False		' To detect redundant calls
+
+		Friend Sub New(ByVal lock As Global.System.Threading.ReaderWriterLockSlim, ByVal mode As LockMode)
+			m_Lock = lock
+			m_Mode = mode
+			Select Case mode
+				Case LockMode.Read
+					m_Lock.EnterReadLock()
+				Case LockMode.Upgradable
+					m_Lock.EnterUpgradeableReadLock()
+				Case LockMode.Write
+					m_Lock.EnterWriteLock()
+			End Select
+		End Sub
+
+		Private Sub Dispose(ByVal disposing As Boolean)
+			If Not Me.m_Disposed Then
+				If disposing Then
+					Select Case m_Mode
+						Case LockMode.Read
+							m_Lock.ExitReadLock()
+						Case LockMode.Upgradable
+							m_Lock.ExitUpgradeableReadLock()
+						Case LockMode.Write
+							m_Lock.ExitWriteLock()
+					End Select
+				End If
+				m_Lock = Nothing
+			End If
+			Me.m_Disposed = True
+		End Sub
+
+		Private Sub Dispose() Implements IDisposable.Dispose
+			Dispose(True)
+		End Sub
+
+	End Class
+
+	Private Enum LockMode
+		Read
+		Upgradable
+		Write
+	End Enum
+
+End Module
+#End If
