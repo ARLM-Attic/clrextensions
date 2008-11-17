86da52a6fc6398f1077293789e8ef2405f51fb6a
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-11-17 01:40:45 +0000

    Created stubs for Compact Framework
    git-svn-id: https://clrextensions.svn.codeplex.com/svn@19312 2327b42d-5241-43d6-9e2a-de5ac946f064

---
+++ ClrExtensionsCore-Mobile-PocketPC-Compact35/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ClrExtensionsCore_Mobile_PocketPC_Compact35")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ClrExtensionsCore_Mobile_PocketPC_Compact35")> 
+<Assembly: AssemblyCopyright("Copyright ©  2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5c08dd40-5872-4479-999f-0d602d409c75")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.*")> 
---
+++ ClrExtensionsCore-Mobile-SmartPhone-Compact35/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ClrExtensionsCore_Mobile_SmartPhone_Compact35")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("ClrExtensionsCore_Mobile_SmartPhone_Compact35")> 
+<Assembly: AssemblyCopyright("Copyright ©  2008")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: CLSCompliant(True)> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("704e3498-d251-4c03-817d-6aa6936d8a5c")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+<Assembly: AssemblyVersion("1.0.*")> 
