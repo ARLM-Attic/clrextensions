a0632f1daff6d3e64d9747388e8e03d892152322
SND\Grauenwolf_cp <SND\Grauenwolf_cp@2327b42d-5241-43d6-9e2a-de5ac946f064>
2008-11-17 06:44:37 +0000

    git-svn-id: https://clrextensions.svn.codeplex.com/svn@19359 2327b42d-5241-43d6-9e2a-de5ac946f064

--- ClrExtensionsCore/System/Net/Rest/QueryParameterCollection.vb
+++ ClrExtensionsCore/System/Net/Rest/QueryParameterCollection.vb
@@ -5,6 +5,13 @@ Namespace Net.Rest
 	Public Class QueryParameterCollection
 		Inherits ObjectModel.Collection(Of QueryParameter)
 
+		Public Sub New()
+
+		End Sub
+		Public Sub New(ByVal queryString As String)
+			AddByParsing(queryString)
+		End Sub
+
 		Public Sub AddRange(ByVal values As System.Collections.Specialized.NameValueCollection)
 			For Each key As String In values.Keys
 				Me.Add(New QueryParameter(key, values(key)))
@@ -15,7 +22,16 @@ Namespace Net.Rest
 			Me.AddRange(ParseQueryString(queryString))
 		End Sub
 
-		Public Shared Function ParseQueryString(ByVal queryString As String) As ObjectModel.Collection(Of QueryParameter)
+		Default Public Overloads ReadOnly Property Item(ByVal key As String) As String
+			Get
+				For Each param In Me
+					If String.Equals(param.Name, key, StringComparison.OrdinalIgnoreCase) Then Return param.Value
+				Next
+				Return Nothing
+			End Get
+		End Property
+
+		Private Shared Function ParseQueryString(ByVal queryString As String) As ObjectModel.Collection(Of QueryParameter)
 			Dim result As New ObjectModel.Collection(Of QueryParameter)
 			Dim rows = queryString.Split("&", StringSplitOptions.RemoveEmptyEntries)
 
